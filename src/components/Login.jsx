68eea2ea895166677304c14501fea88fe1c4c365
Alex Barbosa <alex.andrew.barbosa@gmail.com>
2022-12-06 16:18:51 -0500

    register page setup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,8 @@
 import { useNavigate } from 'react-router-dom'
 import { useState, useEffect, useContext } from 'react'
-import axios from 'axios'
 import React  from 'react'
 import DataContext from './DataContext'
+import '../style/login.css'
 
 const Login = () =>{
 
@@ -31,7 +31,7 @@ const Login = () =>{
       
     }
     const handleSignUp = () =>{
-
+        navigate("/register")
     }
 
   
@@ -45,8 +45,8 @@ const Login = () =>{
             <div className='logo-signup'></div>
             <div className="loginBox">
                 <form>
-                <div className="username"><input className="username" type="username" name="username" onChange={handleChange} value={loggingUser.username} required placeholder="username"></input></div>
-                <div className="password"><input className="pass" type="password" name="password" onChange={handleChange} value={loggingUser.password} required placeholder="Password"></input></div>
+                <div className="userN"><input className="username" type="username" name="username" onChange={handleChange} value={loggingUser.username} required placeholder="username"></input></div>
+                <div className="passW"><input className="password" type="password" name="password" onChange={handleChange} value={loggingUser.password} required placeholder="Password"></input></div>
                 <div className="fPass">Forgot Password?</div>
                 <div className="logButt" onClick={handleLogin}>Log in</div>
                 <div className="signButt" onClick={handleSignUp}>Sign up</div>
---
+++ src/components/Register.jsx
@@ -0,0 +1,41 @@
+import { useNavigate } from 'react-router-dom'
+import { useState, useEffect, useContext } from 'react'
+import React  from 'react'
+import DataContext from './DataContext'
+import '../style/register.css'
+
+const Register = () =>{
+    const iState = {
+        fName:"",
+        lName:"",
+        email:"",
+        password:"",
+        confPass:"",
+    }
+
+    const [uInfo, setuInfo] = useState(iState)
+
+
+    const handleChange = (e) => {
+        setuInfo({ ...uInfo, [e.target.name]: e.target.value });
+    }
+
+    return(
+    <div className='registerPage'>
+        <div className='title'>
+            <div className='wTitle'>WELCOME TO</div>
+            <div className='aTitle'>SCHOLA</div>
+        </div>
+        <div className='genInfo'>
+            <input className='fName' name='fName' onChange={handleChange} value={uInfo.fName} placeholder="First Name"></input>
+            <input className='lName' name='lName' onChange={handleChange} value={uInfo.lName}placeholder="Last Name"></input>
+            <input className='email' name= 'email' onChange={handleChange} value={uInfo.email} placeholder="e-Mail"></input>
+            <input className='regPass' name='password' onChange={handleChange} value={uInfo.password} placeholder="Password"></input>
+            <input className='confPass' name='city' onChange={handleChange} value={uInfo.confPass} placeholder="Confirm Password"></input>
+        </div>
+        <div className='nextButton'>Next</div>
+
+    </div>)
+}
+
+export default Register
