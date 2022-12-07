6fd74c47115cfc2698052b4c7bd72011ca598420
Alex Barbosa <alex.andrew.barbosa@gmail.com>
2022-12-07 15:53:19 -0500

    bchanges

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -13,24 +13,40 @@ const Register = () =>{
         confpass:"",
     }
     const [cPass ,setCpass] = useState('confPass')
-
     const [uInfo, setuInfo] = useState(iState)
-
-
+    const [nButtonClass, setNbuttonClass] = useState("nextButtongrey")
+    const [valid, setValid]  = useState(false)
     const handleChange = (e) => {
-
-
         setuInfo({ ...uInfo, [e.target.name]: e.target.value });
+        checkValid()
+
+        // e.target.value.length < 1 ? setValid(false) : setValid(true) 
+    }
+
+    const checkValid=()=>{
+        //console.log("in check Valid",uInfo)
+        uInfo.confpass === uInfo.password && uInfo.fName.length && uInfo.lName.length && uInfo.email.length && uInfo.password.length && uInfo.confpass.length ? setValid(true) : setValid(false)
 
     }
+
+
     useEffect(()=>{
         // Check for bad password
+
             if(uInfo.confpass === uInfo.password){
                 setCpass('confPass')
             }else{
                 setCpass('badConfPass')
+                setValid(false)
             }
-    },[uInfo])
+            checkValid()
+            console.log("in UE ", uInfo)
+            console.log('Valid is ',valid)
+
+            valid ? setNbuttonClass("nextButton") : setNbuttonClass("nextButtongrey");
+    
+
+    },[uInfo,valid])
 
     return(
     <div className='registerPage'>
@@ -45,7 +61,10 @@ const Register = () =>{
             <input className='regPass' type="password"name='password' onChange={handleChange} value={uInfo.password} placeholder="Password"></input>
             <input className={cPass} type="password" name='confpass' onChange={handleChange} value={uInfo.confPass} placeholder="Confirm Password"></input>
         </div>
-        <div className='nextButton'><div className='nextText'>Next</div></div>
+        <div className="buttons">
+            <div className={nButtonClass}> <div className='nextText'>Next</div> </div>
+        </div>
+        <div className="oldmember">Already a member? <div className="toLogin"> Login here</div></div>
 
     </div>)
 }
