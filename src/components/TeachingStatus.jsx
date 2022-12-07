1c981f8a300d71fc6b332e41e22406976f551020
Ian <imcbee@terpmail.umd.edu>
2022-12-07 18:54:33 -0500

    updates to recentnotification and teachingstatus components

--- src/components/TeachingStatus.jsx
+++ src/components/TeachingStatus.jsx
@@ -1,39 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect, useContext } from "react";
+import { DataContext } from "../components/DataContext";
 import "../style/teachingstatus.css";
-import { useState } from "react";
-import { useEffect } from "react";
+
+const DB_URL = "https://dsh-backend.fly.dev";
 
 const TeachingStatus = () => {
+  //! ------------------------useContext---------------------------
+  const { currentUser } = useContext(Context);
+
   //! ------------------------useState---------------------------
   const [statData, setStatData] = useState();
 
   const getStatData = async () => {
     try {
-      const response = await fetc;
+      const response = await fetch(`${DB_URL}/user/${currentUser.user._id}`);
+      const data = await response.json();
+      setStatData(data);
     } catch (err) {
       console.log(err);
     }
   };
 
-  //!  ------------------------useEffect---------------------------
-  useEffect(() => {}, []);
-
   return (
     <div className="teachingstat-container">
       <div className="stat">
         {/* number of students */}
-        <h1></h1>
+        <h1>5</h1>
+        {console.log(statData)}
         {/* students */}
+        <p>Students</p>
       </div>
       <div className="stat">
         {/* number hours tutored */}
-        <h1></h1>
+        <h1>146</h1>
         {/* hours tutored */}
+        <p>Hours Tutored</p>
       </div>
       <div className="stat">
         {/* number of reviews */}
-        <h1></h1>
+        <h1>15</h1>
         {/* reviews */}
+        <p>Reviews</p>
       </div>
     </div>
   );
