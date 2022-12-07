15188076e6b01c921c17c3b17d7d85c0fff3c551
Ian <imcbee@terpmail.umd.edu>
2022-12-07 12:53:14 -0500

    css format on navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,10 @@
 import "../style/navbar.css";
 import { Link } from "react-router-dom";
+import home from "../assets/icons/home.svg";
+import calendar from "../assets/icons/calendar.svg";
+import person from "../assets/icons/Profile.svg";
+import notifications from "../assets/icons/notifs.svg";
+import mail from "../assets/icons/messages.svg";
 
 const NavBar = () => {
   return (
@@ -7,23 +12,33 @@ const NavBar = () => {
       <nav className="navbar">
         {/* Home Icon */}
         <Link to="/main">
-          <button className="nav-button">Home</button>
+          <button className="nav-button">
+            <img src={home} alt="home" className="filter-orange" />
+          </button>
         </Link>
         {/* Calendar Icon */}
         <Link to="">
-          <button className="nav-button">Calendar</button>
+          <button className="nav-button">
+            <img src={calendar} alt="home" className="filter-orange" />
+          </button>
         </Link>
         {/* Person Icon */}
         <Link to="">
-          <button className="nav-button">Person</button>
+          <button className="nav-button">
+            <img src={person} alt="home" className="filter-orange" />
+          </button>
         </Link>
         {/* Notification Icon */}
         <Link to="">
-          <button className="nav-button">Notify</button>
+          <button className="nav-button">
+            <img src={notifications} alt="home" className="filter-orange" />
+          </button>
         </Link>
         {/* Mail Icon */}
         <Link to="">
-          <button className="nav-button">Mail</button>
+          <button className="nav-button">
+            <img src={mail} alt="home" className="filter-orange" />
+          </button>
         </Link>
       </nav>
     </div>
