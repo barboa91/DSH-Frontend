87692cd9a3a531c6319fe957d39a02ccc32e7a73
Alex Barbosa <alex.andrew.barbosa@gmail.com>
2022-12-07 21:05:31 -0500

    added login page with style

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,25 +2,32 @@ import { useState } from "react";
 import fb from '../assets/icons/Facebook.png';
 import gg from '../assets/icons/Google.png';
 import li from '../assets/icons/LinkedIn.png';
+import '../style/login.css'
 
 const Login = () => {
+
+    const handleChange= (e)=>{
+        console.log('do stuff')
+    }
     return (
         <div>
-        <h1>SCHOLA</h1>
-        <h2>Next Gen Education</h2>
-        <form>
-        <input className='loginUsername' placeholder='Username' onChange={handleChange}></input>
-        <input className='loginPassword' placeholder='Password' onChange={handleChange}></input>
-        <hr>Or log in with</hr>
-        <div className="loginImages">
-            <img src={gg} alt="Google icon"></img>
-            <img src={fb} alt="Facebook icon"></img>
-            <img src={li} alt="LinkedIn icon"></img>
-        </div>
-        <button>Login</button>
-        </form>
-        <p>Not a member? Sign-up here</p>
-        <div>Forgot username/password?</div>
+            <div className="aTitle">SCHOLA</div>
+            <div className="nge">Next Gen Education</div>
+            <form>
+            <input className='loginUsername' placeholder='Username' onChange={handleChange}></input>
+            <input className='loginPassword' placeholder='Password' onChange={handleChange}></input>
+            <div className="way" >Or log in with</div>
+            <div className="loginImages" >
+                <img src={gg} alt="Google icon"></img>
+                <img src={fb} alt="Facebook icon"></img>
+                <img src={li} alt="LinkedIn icon"></img>
+            </div>
+            <div className="logButton">Login</div>
+            </form>
+            <div> </div>
+        <div className="oldmember">Not a member? <div className="toLogin"> Sign-up here</div></div>
+
+            <div className="unpw">Forgot username/password?</div>
         </div>
     )
 }
